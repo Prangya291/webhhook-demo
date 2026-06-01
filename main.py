3baeb995c073acf1f5d828e9e8bc3a8e83a9abe5
Prangya291 <prangyasreepatanayak@gmail.com>
2026-06-01 16:35:01 +0530

    Webhook working

--- main.py
+++ main.py
@@ -11,10 +11,19 @@ def home():
 @app.post("/webhook")
 async def github_webhook(request: Request):
 
+    body = await request.body()
+
+    print("\n========== REQUEST RECEIVED ==========")
+
+    if not body:
+        print("Empty body received")
+        return {"status": "received"}
+
     payload = await request.json()
 
-    print("\n========== GITHUB WEBHOOK ==========")
+    event = request.headers.get("X-GitHub-Event")
 
+    print("GitHub Event:", event)
     print(payload)
 
     return {"status": "received"}
